d5e7bb110a64c763046502b20dccf9b3c0c54357
Khon Piseth <pisethkhon888@gmail.com>
2025-10-01 03:24:40 +0000

    first file

--- autopush.sh
+++ autopush.sh
@@ -1,27 +1,27 @@
+# #!/bin/bash
+
+# # Commit & push submodules first
+# git submodule foreach '
+#   echo "🚀 Committing in submodule $name"
+#   git add .
+#   git commit -m "Update submodule $name" || true
+#   git push origin $(git branch --show-current) || true
+# '
+
+# # Commit & push main repo
+# git add .
+# git commit -m "$1" || true
+# git pull --rebase origin master
+# git push origin master
+
+
 #!/bin/bash
 
-# Commit & push submodules first
-git submodule foreach '
-  echo "🚀 Committing in submodule $name"
-  git add .
-  git commit -m "Update submodule $name" || true
-  git push origin $(git branch --show-current) || true
-'
-
-# Commit & push main repo
-git add .
-git commit -m "$1" || true
-git pull --rebase origin master
-git push origin master
-
-
-#!/bin/bash
-
-# message="$1"
-# if [ -z "$message" ]; then 
-#     echo "usage: bash $0 <commit-message>" 
-#     exit 1 
-# fi 
-# git add . 
-# git commit -m "$message" 
-# git push -u origin $(git branch --show-current)
+message="$1"
+if [ -z "$message" ]; then 
+    echo "usage: bash $0 <commit-message>" 
+    exit 1 
+fi 
+git add . 
+git commit -m "$message" 
+git push -u origin $(git branch --show-current)
